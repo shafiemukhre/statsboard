de779f1a5cbbf301cef30ec182227bf041554926
rutujahasurkar <hasurkarrutuja@gmail.com>
2020-04-26 20:51:18 -0700

    Add files via upload

---
+++ frontend/src/components/pages/Notebook.js
@@ -0,0 +1,48 @@
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { Grid } from '@material-ui/core';
+import ReactDOM from "react-dom";
+import AddIcon from '@material-ui/icons/Add';
+import CreateIcon from '@material-ui/icons/Create';
+import TitleIcon from '@material-ui/icons/Title';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+
+
+export default function Notebook() {
+	
+	
+  return (
+    <div>
+        <Grid item xs={12} sm={12}>
+			<div>
+				<paper>
+					Notebook-1
+				</paper>
+			</div> 
+			<div>
+				<div align="right">
+					<IconButton color="primary" aria-label="add key value pair">
+					<AddIcon  fontSize="large" />
+					</IconButton>
+				</div>
+				<div align="right">
+					<IconButton color="primary" aria-label="add text">
+					<TitleIcon  fontSize="large" />
+					</IconButton>
+				</div>
+				<div align="right">
+					<IconButton color="primary" aria-label="add python script">
+					<CreateIcon  fontSize="medium" />
+					</IconButton>
+				</div>	
+			</div>
+        </Grid>
+    </div>
+  );
+}
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Notebook />, rootElement);
