6c3099e094204a5dd321ad36dfe0f98fb951a4c0
shafie <shafiemukhre@gmail.com>
2020-05-25 01:38:49 -0700

    add store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,2 +1,13 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+
+export const languageContext = createContext()
+export default function Store(props){
+    const [language, setLanguage] = useState('english')
+
+    return(
+        <languageContext.Provider value={[language, setLanguage]}>
+            {props.children}
+        </languageContext.Provider>
+    )
+}
 
